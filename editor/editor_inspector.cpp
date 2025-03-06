2cf7e07bc322adc43f826f13a40b2afd629910af
Jonny K <J.Kenyon@ordinarygizmos.com>
2025-03-06 08:47:02 -0600

    Add Spine Module

--- editor/editor_inspector.cpp
+++ editor/editor_inspector.cpp
@@ -48,6 +48,7 @@
 #include "editor/plugins/script_editor_plugin.h"
 #include "editor/themes/editor_scale.h"
 #include "editor/themes/editor_theme_manager.h"
+#include "scene/gui/box_container.h"
 #include "scene/gui/margin_container.h"
 #include "scene/gui/separator.h"
 #include "scene/gui/spin_box.h"
@@ -4008,7 +4009,7 @@ Object *EditorInspector::get_next_edited_object() {
 }
 
 void EditorInspector::edit(Object *p_object) {
-	if (object == p_object) {
+	if (object == p_object || lock_panel_switching) {
 		return;
 	}
 
@@ -4886,6 +4887,24 @@ Variant EditorInspector::get_property_clipboard() const {
 	return property_clipboard;
 }
 
+void EditorInspector::_pin_button_toggled(bool p_pressed) {
+    lock_panel_switching = p_pressed;
+}
+
+void EditorInspector::_check_meta_name() {
+	const String meta_name = add_meta_name->get_text();
+
+	if (meta_name.is_empty()) {
+		validation_panel->set_message(EditorValidationPanel::MSG_ID_DEFAULT, TTR("Metadata name can't be empty."), EditorValidationPanel::MSG_ERROR);
+	} else if (!meta_name.is_valid_identifier()) {
+		validation_panel->set_message(EditorValidationPanel::MSG_ID_DEFAULT, TTR("Metadata name must be a valid identifier."), EditorValidationPanel::MSG_ERROR);
+	} else if (object->has_meta(meta_name)) {
+		validation_panel->set_message(EditorValidationPanel::MSG_ID_DEFAULT, vformat(TTR("Metadata with name \"%s\" already exists."), meta_name), EditorValidationPanel::MSG_ERROR);
+	} else if (meta_name[0] == '_') {
+		validation_panel->set_message(EditorValidationPanel::MSG_ID_DEFAULT, TTR("Names starting with _ are reserved for editor-only metadata."), EditorValidationPanel::MSG_ERROR);
+	}
+}
+
 void EditorInspector::_show_add_meta_dialog() {
 	if (!add_meta_dialog) {
 		add_meta_dialog = memnew(AddMetadataDialog());
@@ -4948,6 +4967,17 @@ EditorInspector::EditorInspector() {
 	base_vbox = memnew(VBoxContainer);
 	base_vbox->set_h_size_flags(SIZE_EXPAND_FILL);
 	add_child(base_vbox);
+    
+	HBoxContainer *subresource_hb = memnew(HBoxContainer);
+    subresource_hb->set_h_size_flags(Control::SIZE_EXPAND_FILL);
+    base_vbox->add_child(subresource_hb);
+    pin_button = memnew(Button);
+	subresource_hb->add_child(pin_button);
+	pin_button->set_theme_type_variation("FlatMenuButton");
+	pin_button->set_toggle_mode(true);
+    pin_button->set_text("Pin");
+	pin_button->set_tooltip_text(TTR("Pin Inspected Node"));
+	pin_button->connect(SNAME("toggled"), callable_mp(this, &EditorInspector::_pin_button_toggled));
 
 	begin_vbox = memnew(VBoxContainer);
 	base_vbox->add_child(begin_vbox);
@@ -4973,7 +5003,6 @@ EditorInspector::EditorInspector() {
 
 	main_vbox = memnew(VBoxContainer);
 	base_vbox->add_child(main_vbox);
-
 	set_horizontal_scroll_mode(SCROLL_MODE_DISABLED);
 
 	changing = 0;
