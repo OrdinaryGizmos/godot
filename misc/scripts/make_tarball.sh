d8b8c7599618bc767bd1cd34f3021b8abbd25b5a
AtlasMap <atlasmap-dev@redhat.com>
2026-02-02 17:35:39 -0600

    Update Local scripts

--- misc/scripts/make_tarball.sh
+++ misc/scripts/make_tarball.sh
@@ -52,7 +52,7 @@ echo
 echo "The tarball will be written to the parent folder:"
 echo "    $(dirname $CURDIR)/$NAME.tar.gz"
 
-git archive $HEAD --prefix=$NAME/ -o $TMPDIR/$NAME.tar
+git-archive-all --prefix=$NAME/ $TMPDIR/$NAME.tar
 
 # Adding custom .git/HEAD to tarball so that we can generate GODOT_VERSION_HASH.
 cd $TMPDIR
